a08eb660204a4bbe202d1a11c301f18c9a998c06
Alberto Batty <alberto.batty@gmail.com>
2019-04-14 18:37:58 -0500

    Changed temperature to C

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -15,9 +15,14 @@ const forecast = (latitude, longitude, callback) => {
         else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. With a lowest temperature of ' + body.daily.data[0].temperatureMin + ', and a highest temperature of ' + body.daily.data[0].temperatureMax+ '. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + changeFtoC(body.currently.temperature) + ' degress out. With a lowest temperature of ' + changeFtoC(body.daily.data[0].temperatureMin) + ', and a highest temperature of ' + changeFtoC(body.daily.data[0].temperatureMax) + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
+}
+
+var changeFtoC = (temperatureInF) => {
+    var temperaturInC = (temperatureInF- 32)*(5/9)
+    return temperaturInC.toFixed(2)
 }
 
 module.exports = forecast
