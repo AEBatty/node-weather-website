e856a5e20fec53a42b222088da7cc8cc9fe3588d
Alberto Batty <alberto.batty@gmail.com>
2019-04-14 18:27:51 -0500

    Change message at weather

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -15,7 +15,7 @@ const forecast = (latitude, longitude, callback) => {
         else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. With a lowest temperature of ' + body.daily.data[0].temperatureMin + ', and a highest temperature of ' + body.daily.data[0].temperatureMax+ '. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
